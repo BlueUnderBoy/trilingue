7c7d5e51131fca620c57acee65fa72d808615bb4
Jalen Young <lital5@yahoo.com>
2024-08-21 03:38:25 -0500

    SBA_316

--- trilingue.js
+++ trilingue.js
@@ -1,17 +1,109 @@
-import "/trilingue.css"
+let su = document.getElementById("su")
+let reg = document.getElementById("reg")
 
-const signup = document.getElementById("su")
-const body = document.querySelector(".lbody")
-let loreg = createElement("div")
+let loreg = document.createElement("div")
+let tdiv = document.createElement("div")
+let ltitle = document.createElement("h1")
+let lcont = document.createElement("div")
+let fdivun = document.createElement("div")
+let fdivem = document.createElement("div")
+let fdivpw = document.createElement("div")
+let fdivpwv = document.createElement("div")
+let fdivsub = document.createElement("div")
+let lform = document.createElement("form")
+let labelun = document.createElement("label")
+let linputun = document.createElement("input")
+let labelem = document.createElement("label")
+let linputem = document.createElement("input")
+let labelpw = document.createElement("label")
+let linputpw = document.createElement("input")
+let labelvpw = document.createElement("label")
+let linputvpw = document.createElement("input")
+let linputsub = document.createElement("input")
 
-body.appendChild(loreg)
-loreg.setAttribute("class", "overlay")
-loreg.style.setProperty("height", "200px")
-loreg.style.display = "none"
+loreg.setAttribute("id", "loreg")
+tdiv.setAttribute("id", "tdiv")
+ltitle.setAttribute("id", "ltitle")
+lcont.setAttribute("id", "lcont")
+fdivun.setAttribute("id", "fdivun")
+fdivem.setAttribute("id", "fdivem")
+fdivpw.setAttribute("id", "fdivpw")
+fdivpwv.setAttribute("id", "fdivpwv")
+fdivsub.setAttribute("id", "fdivsub")
+lform.setAttribute("id", "lform")
+labelun.setAttribute("id", "labelun")
+labelem.setAttribute("id", "labelem")
+labelpw.setAttribute("id", "labelpw")
+labelvpw.setAttribute("id", "labelvpw")
+linputun.setAttribute("id", "linputun")
+linputem.setAttribute("id", "linputem")
+linputem.setAttribute("type", "email")
+linputpw.setAttribute("id", "linputpw")
+linputvpw.setAttribute("id", "linputvpw")
+linputsub.setAttribute("id", "linputsub")
+linputsub.setAttribute("type", "submit")
+linputsub.setAttribute("name", "Register")
 
-signup.addEventListener('click', function(event) {
-    event.preventDefault()
-    loreg.style.display = block;
+
+su.addEventListener("click", function(){
+    reg.appendChild(loreg)
+    loreg.appendChild(tdiv)
+    loreg.appendChild(lcont)
+    lcont.appendChild(lform)
+    lform.appendChild(fdivun)
+    lform.appendChild(fdivem)
+    lform.appendChild(fdivpw)
+    lform.appendChild(fdivpwv)
+    lform.appendChild(fdivsub)
+    tdiv.appendChild(ltitle)
+    fdivun.appendChild(labelun)
+    fdivun.appendChild(linputun)
+    fdivem.appendChild(labelem)
+    fdivem.appendChild(linputem)
+    fdivpw.appendChild(labelpw)
+    fdivpw.appendChild(linputpw)
+    fdivpwv.appendChild(labelvpw)
+    fdivpwv.appendChild(linputvpw)
+    fdivsub.appendChild(linputsub)
+    ltitle.innerHTML = "Create an account!"
+    labelun.innerHTML = "Username"
+    labelem.innerHTML = "Email"
+    labelpw.innerHTML = "Password"
+    labelvpw.innerHTML = "Verify Password"
+    console.log(loreg.textContent)
     loreg.style.setProperty("position", "absolute")
+    loreg.style.setProperty("background-color", "white")
+    loreg.style.setProperty("width", "500px")
+    loreg.style.setProperty("height", "500px")
+    tdiv.style.setProperty("border-style", "groove")
+    ltitle.style.setProperty("width", "270px")
+    //CSS
+    let form_css = document.querySelector("#lform")
+    form_css.style.setProperty("width", "350px")
 
-})
+    let title_css = document.querySelector("#tdiv")
+    title_css.style.setProperty("display", "flex")
+    title_css.style.setProperty("justify-content", "center")
+
+    let fdivun_css = document.querySelector("#fdivun")
+    fdivun_css.style.setProperty("display", "flex")
+    fdivun_css.style.setProperty("justify-content", "space-between")
+    
+    let fdivem_css = document.querySelector("#fdivem")
+    fdivem_css.style.setProperty("display", "flex")
+    fdivem_css.style.setProperty("justify-content", "space-between")
+
+    let fdivpw_css = document.querySelector("#fdivpw")
+    fdivpw_css.style.setProperty("display", "flex")
+    fdivpw_css.style.setProperty("justify-content", "space-between")
+
+    let fdivpwv_css = document.querySelector("#fdivpwv")
+    fdivpwv_css.style.setProperty("display", "flex")
+    fdivpwv_css.style.setProperty("justify-content", "space-between")
+
+    let fdivsub_css = document.querySelector("#fdivsub")
+    fdivsub_css.style.setProperty("display", "flex")
+    fdivsub_css.style.setProperty("justify-content", "center")
+    
+});
+
